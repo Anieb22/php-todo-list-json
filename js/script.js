7d5a361d22ed0b36834feb44d4106cf5a7d796af
Anieb22 <anielloverdino@gmail.com>
2023-07-06 18:54:50 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            messaggioProva: 'Hello Vue!'
+        }
+    },
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+
+}).mount('#app')
